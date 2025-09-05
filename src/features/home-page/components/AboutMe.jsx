ddd808ca970a4bcb1e158051ecfed2545f80bcbc
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-09-06 00:17:42 +0800

    [DEV] About me passed

--- src/features/home-page/components/AboutMe.jsx
+++ src/features/home-page/components/AboutMe.jsx
@@ -3,33 +3,43 @@ import DustyChar from '../../../assets/projects/DustyChar.png'
 
 export default function AboutMe() {
   return (
-    <div className='bg-white '>
-        <h1 className='text-4xl px-4'>DustyHansCS</h1>
-        <div className='flex items-center justify-around'>
-          <div className='h-full w-[20%] bg-red-100'>
-            <img src={DustyChar} alt="" />
+    <div className="bg-white">
+      <h1 className="text-4xl px-4 mb-6">DustyHansCS</h1>
+
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-around gap-6 px-4">
+        <div className="w-full bg-red-100 md:w-1/4 flex justify-center md:justify-start">
+          <img src={DustyChar} alt="" className="max-h-60 object-contain" />
+        </div>
+        <div className="w-full md:w-3/4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="flex flex-col items-center text-center">
+            <div className="h-40 w-40 bg-blue-200 flex items-center justify-center rounded-lg overflow-hidden">
+              <img src={DustyChar} alt="" className="h-full object-contain" />
+            </div>
+            <p className="text-gray-700 font-semibold mt-2">
+              Currently pursuing a degree in Computer Science
+            </p>
           </div>
-          <div className='w-[60%] flex justify-around py-5 gap-5'>
-            <div className='w-1/3'>
-              <div className='h-[10rem] w-[10rem] bg-blue-200'>
-              <img src={DustyChar} alt="" />
-              </div>
-              <p className='text-gray-700 font-semibold'>Currently pursuing a degree in Computer Science</p>
+
+          <div className="flex flex-col items-center text-center">
+            <div className="h-40 w-40 bg-blue-200 flex items-center justify-center rounded-lg overflow-hidden">
+              <img src={DustyChar} alt="" className="h-full object-contain" />
             </div>
-            <div className='w-1/3'>
-              <div className='h-[10rem] w-[10rem] bg-blue-200'>
-                <img src={DustyChar} alt="" />
-              </div>
-              <p className='text-gray-700 font-semibold'>Experienced in web development and game development</p>
+            <p className="text-gray-700 font-semibold mt-2">
+              Experienced in web development and game development
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center text-center">
+            <div className="h-40 w-40 bg-blue-200 flex items-center justify-center rounded-lg overflow-hidden">
+              <img src={DustyChar} alt="" className="h-full object-contain" />
             </div>
-            <div className='w-1/3'> 
-              <div className='h-[10rem] w-[10rem] bg-blue-200'>
-                <img src={DustyChar} alt="" />
-              </div>
-              <p className='text-gray-700 font-semibold'>Passionate about creating engaging and interactive experiences</p>
-            </div>
+            <p className="text-gray-700 font-semibold mt-2">
+              Passionate about creating engaging and interactive experiences
+            </p>
           </div>
         </div>
+      </div>
     </div>
+
   )
 }
--- src/features/home-page/pages/HomePage.jsx
+++ src/features/home-page/pages/HomePage.jsx
@@ -88,7 +88,7 @@ export default function HomePage() {
     <div className='h-full'>
         <ComponentSlider slides={[Introduction, LatestProject, LatestGame]}/>
         <div className='w-[100%] m-auto bg-gray-100 gap-5'>
-          <div className='sm:w-[95vw] :m-auto grid grid-cols-1 md:grid-cols-[2fr_1fr] sm:gap-2'>
+          <div className='sm:w-[95vw] m-auto grid grid-cols-1 md:grid-cols-[2fr_1fr] sm:gap-2'>
             <LatestRelease />
             <Portfoli />
           </div>
