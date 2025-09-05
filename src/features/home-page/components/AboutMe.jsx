70ca49a947a9422d30dc246631013d26b86d878a
DustyC <123617229+DustyCs@users.noreply.github.com>
2025-09-05 19:46:48 +0800

    [Update] Fixed about me page

--- src/features/home-page/components/AboutMe.jsx
+++ src/features/home-page/components/AboutMe.jsx
@@ -5,28 +5,28 @@ export default function AboutMe() {
   return (
     <div className='bg-white '>
         <h1 className='text-4xl px-4'>DustyHansCS</h1>
-        <div className='flex items-end gap-2'>
-          <div className='h-full w-[30%] bg-red-100 flex items-end justify-center'>
+        <div className='flex items-center justify-around'>
+          <div className='h-full w-[20%] bg-red-100'>
             <img src={DustyChar} alt="" />
           </div>
-          <div className='w-[70%] flex justify-around gap-5'>
+          <div className='w-[60%] flex justify-around py-5 gap-5'>
             <div className='w-1/3'>
               <div className='h-[10rem] w-[10rem] bg-blue-200'>
               <img src={DustyChar} alt="" />
               </div>
-              <p>Currently pursuing a degree in Computer Science</p>
+              <p className='text-gray-700 font-semibold'>Currently pursuing a degree in Computer Science</p>
             </div>
             <div className='w-1/3'>
               <div className='h-[10rem] w-[10rem] bg-blue-200'>
                 <img src={DustyChar} alt="" />
               </div>
-              <p>Experienced in web development and game development</p>
+              <p className='text-gray-700 font-semibold'>Experienced in web development and game development</p>
             </div>
             <div className='w-1/3'> 
               <div className='h-[10rem] w-[10rem] bg-blue-200'>
                 <img src={DustyChar} alt="" />
               </div>
-              <p>Passionate about creating engaging and interactive experiences</p>
+              <p className='text-gray-700 font-semibold'>Passionate about creating engaging and interactive experiences</p>
             </div>
           </div>
         </div>
